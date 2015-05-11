36034b59df4fe334bf63166415572486f1dfd3e3
joelrahman <joel.rahman.home@gmail.com>
2015-05-12 09:26:32 +1000

    Update metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,8 @@
+name             "rvm_passenger"
 maintainer       "Fletcher Nichol"
 maintainer_email "fnichol@nichol.ca"
 license          "Apache 2.0"
-description      "Installs and configures Passenger under RVM with Apache or nginx"
+description      "Installs and configues Passenger under RVM with Apache or nginx"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.8.3"
 
